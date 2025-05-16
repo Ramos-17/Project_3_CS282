f962de3cdd16ed87e41ffc19ea69e1e0af647515
Ramos-17 <gramos0617@gmail.com>
2025-05-15 19:09:23 -0700

    Create tour.java

---
+++ tour.java
@@ -0,0 +1,8 @@
+/*
+Tour Planets – Class: Tour
+Type of graph: Weighted directed, same data as travel
+Goal: Visit all planets exactly once and return to the starting planet with the least cost.
+Algorithm hint: This is the Traveling Salesman Problem (TSP). You might use backtracking, dynamic programming, or heuristics to approximate a solution, since TSP is NP-hard.
+Input: travel.txt
+Output: A complete round-trip path and the total tour time.
+*/
