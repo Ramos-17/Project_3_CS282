065890eaf4e005a40bbe2e2dbcc1fa4817fb8c79
Ramos-17 <gramos0617@gmail.com>
2025-05-15 19:08:13 -0700

    Create travel.java

---
+++ travel.java
@@ -0,0 +1,8 @@
+/*
+Quick Travel – Class: Travel
+Type of graph: Weighted directed
+Goal: From a starting planet to a destination planet, find the fastest path (least total weight).
+Algorithm hint: This is a Single-Source Shortest Path problem — you should use Dijkstra’s algorithm.
+Input: travel.txt – contains directed edges like Earth Mars 50.
+Output: The path from source to destination and the total travel time.
+*/
